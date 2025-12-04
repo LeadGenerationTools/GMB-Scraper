b5016d9ec084c6b028c2ffbaf1cd5dc6fbb4519c
LeadGenerationTools <efficiencybooster537@gmail.com>
2025-12-05 00:14:46 +0800

    fix bugs

--- contentScript2.js
+++ contentScript2.js
@@ -1,1 +1,1 @@
-(()=>{if("lcl"===(new URLSearchParams(window.location.search)).get("tbm")){console.log("inject new script.");var a=document.createElement("script");a.src=chrome.runtime.getURL("injected.js");a.onload=function(){this.remove()};(document.head||document.documentElement).appendChild(a)}})();
+(()=>{var a=document.createElement("script");a.src=chrome.runtime.getURL("injected.js");a.onload=function(){this.remove()};(document.head||document.documentElement).appendChild(a);console.log("inject new script.")})();
